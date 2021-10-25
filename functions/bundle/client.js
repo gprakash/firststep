bbacfda2d292092cd54923993c041d0ccbbbee85
gprakash <guruprakash.cr@gmail.com>
2021-10-25 16:14:28 -0400

    In Mem Cache

--- functions/bundle/client.js
+++ functions/bundle/client.js
@@ -1,7 +1,7 @@
 const ApolloClient = require('apollo-boost').ApolloClient;
 const createHttpLink = require('apollo-link-http').createHttpLink;
 const fetch = require('cross-fetch/polyfill').fetch;
-const cache = require('apollo-cache-inmemory').InMemoryCache;
+const inMemCache = require('apollo-cache-inmemory').InMemoryCache;
 const link = createHttpLink({uri: '/.netlify/functions/graphql', fetch });
 
 const cache = new InMemoryCache({
