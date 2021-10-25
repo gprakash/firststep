f8d1c70c63433e35c2f9a336649516034bbdd8ad
gprakash <guruprakash.cr@gmail.com>
2021-10-25 13:48:38 -0400

    New client

--- functions/bundle/client.js
+++ functions/bundle/client.js
@@ -1,1 +1,10 @@
-asas
+const ApolloClient = require('apollo-boost').ApolloClient;
+
+function createClient () {
+        return new ApolloClient({
+        uri: "/.netlify/functions/graphql"
+    });
+}
+
+
+module.exports = { createClient }
